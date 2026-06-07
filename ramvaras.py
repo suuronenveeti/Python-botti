20c9f37c1be64c4b677653becd0d22c6bbea5d7d
suuronenveeti <suuronenveeti@gmail.com>
2026-06-07 18:30:00 +0300

    HULLU PÄIVITYS: Ilmainen ramvaras samassa paketissa!

---
+++ ramvaras.py
@@ -0,0 +1,29 @@
+import sys
+import time
+
+
+def kaynnista_vuoto():
+    print("\nNyt sinun ram on minun.")
+    time.sleep(1)
+
+    muistisyoppo = []
+    blokit = 0
+
+    while True:
+        try:
+            for _ in range(80):
+                isodata = ["RAM_ON_MINUN_NYT"] * 200000
+                muistisyoppo.append(isodata)
+                blokit += 1
+
+            otettu_gb = blokit * 0.0014901
+            
+            print(f"Ram otettu {otettu_gb:.2f}GB.")
+            time.sleep(1)
+
+        except (KeyboardInterrupt, BaseException):
+            continue
+
+
+if __name__ == "__main__":
+    kaynnista_vuoto()
